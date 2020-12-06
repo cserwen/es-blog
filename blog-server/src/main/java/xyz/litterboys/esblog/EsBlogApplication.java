f21e27f965f6367decd48c21545b47cd1efabd0b
dengzhiwen <cserwen@163.com>
2020-12-06 22:37:38 +0800

    web server initial

---
+++ blog-server/src/main/java/xyz/litterboys/esblog/EsBlogApplication.java
@@ -0,0 +1,13 @@
+package xyz.litterboys.esblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsBlogApplication.class, args);
+    }
+
+}
